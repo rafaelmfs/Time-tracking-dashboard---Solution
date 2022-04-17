2de37369508b0e20962ef46207735e28814bf669
Rafael Marques <rafael.marques-98@hotmail.com>
2022-04-17 16:36:50 -0300

    Finalizando

--- src/components/TrackingCard/index.jsx
+++ src/components/TrackingCard/index.jsx
@@ -7,11 +7,11 @@ export const TrackingCard = ({ className, title, content, setPeriod, period }) =
     <div className={className}>
       <div className="card-info">
         <header>
-          <h4>{title}</h4>
+          <h1>{title}</h1>
           <PeriodList setPeriod={setPeriod} />
         </header>
         <div className="card-content">
-          <h1 className="card-value">{content.current}hrs</h1>
+          <h2 className="card-value">{content.current}hrs</h2>
           {period == 'daily' && <p className="prev-value">Yesterday - {content.previous}hrs</p>}
           {period == 'weekly' && <p className="prev-value">Last Week - {content.previous}hrs</p>}
           {period == 'monthly' && <p className="prev-value">Last Month - {content.previous}hrs</p>}
--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -5,8 +5,8 @@ export const UserInfo = () => {
     <div className="user-info">
       <img src="./images/image-jeremy.png" alt="a thumbnail of the user's photo is displayed" />
       <div>
-        <p>Report for</p>
-        <h1>Jeremy Robson</h1>
+        <h1>Report for</h1>
+        <h2>Jeremy Robson</h2>
       </div>
     </div>
   );
